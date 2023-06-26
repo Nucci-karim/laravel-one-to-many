cfdb8c063dfec6cf01873b96d616ceee5c7f07e8
Nucci-karim <nuccikarim@gmail.com>
2023-06-26 09:47:12 +0200

    primo push

---
+++ app/Http/Controllers/admin/AdminController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    //    $posts = Project::All();
+    //    return view ('admin.index', compact('posts'));
+
+        return view ('dashboard');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view( 'admin.create' );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required|unique:projects|max:100'
+        ],
+        [
+           'title.required'=>'Il campo è obbligatorio',
+           'title.unique'=>'Il dato è già presente',
+           'title-max'=>'Il titolo supera il valore massimo' 
+        ]);
+
+        $form_data = $request->all();
+
+        $new_project = new Project();
+
+        $new_project->fill( $form_data );
+        
+                $new_project->save();
+        
+                return redirect()->route( 'admin.index.index' );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+        $project = Project::find($id);
+        return view('admin.show', compact( 'project' ));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Admin\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\UserController;
+use App\Http\Controllers\admin\ProjectController;
+use App\Http\Controllers\admin\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::resource('/', UserController::class);
+
+
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'] )->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); 
+});
+
+require __DIR__.'/auth.php';
